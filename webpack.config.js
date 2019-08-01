6ec9a76fceda668e359ff6902a5ee2f217c216c9
Valery Shibanov <shibaon@gmail.com>
2019-08-01 17:11:03 +0300

    Building config

--- webpack.config.js
+++ webpack.config.js
@@ -3,17 +3,9 @@ const webpack = require('webpack')
 const package = require('./package.json')
 
 module.exports = {
-    entry: './src/index.ts',
+    entry: './dist/index.js',
     mode: 'production',
     devtool: 'inline-source-map',
-    module: {
-        rules: [
-            { test: /\.tsx?$/, use: 'ts-loader', exclude: /node_modules/ },
-        ],
-    },
-    resolve: {
-        extensions: [ '.tsx', '.ts', '.js' ]
-    },
     output: {
         filename: 'native-elements-carousel.min.js',
         path: path.resolve(__dirname, 'dist')
